b3f3544237c40ff1f2b7cfe71db27a9d2c8ae031
sunny <hewei@imasenwh.com>
2025-11-18 11:50:47 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # In setup.py
 
 import os
-import sys
+import sys  # <--- 确保导入 sys
 import platform
 import subprocess
 from setuptools import setup, Extension
@@ -16,10 +16,14 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 
-
+        # ------------------- 这是关键的修复 -------------------
+        # 我们需要告诉 CMake 在哪里可以找到由 pip 安装的包 (pybind11, numpy)
+        # sys.prefix 指向当前 Python 环境的根目录
         cmake_args = [
             f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
+            f'-DCMAKE_PREFIX_PATH={sys.prefix}'  # <--- 添加这一行
         ]
+        # ----------------------------------------------------
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
@@ -38,18 +42,15 @@ class CMakeBuild(build_ext):
         
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-
-        # 注意：我们移除了 -Dpybind11_DIR 和 -DPYTHON_EXECUTABLE
+        
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
+# setup(...) 部分保持不变
 setup(
     name='shape_based_matching_py',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A Python wrapper for shape based matching',
-    long_description='',
+    # ... 其他元数据 ...
     ext_modules=[CMakeExtension('shape_based_matching_py')],
     cmdclass={'build_ext': CMakeBuild},
     zip_safe=False,
