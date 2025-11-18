11d6f9587ccccbf61d281404f4fc71cc24b4e3b3
sunny <hewei@imasenwh.com>
2025-11-18 11:46:25 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,77 +1,57 @@
+# In setup.py
 
 import os
-import re
 import sys
 import platform
 import subprocess
-
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import pybind11
-from distutils.version import LooseVersion
-
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
-
 class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
-        try:
-            pybind11_cmake_dir = pybind11.cmake_dir
-        # 如果失败，则回退到使用旧的 get_cmake_dir() 函数
-        except AttributeError:
-            pybind11_cmake_dir = pybind11.get_cmake_dir()
-        cmake_args.append(f'-Dpybind11_DIR={pybind11_cmake_dir}')
+
+
+        cmake_args = [
+            f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
+        ]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
+        cmake_args += [f'-DCMAKE_BUILD_TYPE={cfg}']
 
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            cmake_args += [f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}']
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
             build_args += ['--', '/m']
         else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             build_args += ['--', '-j2']
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
+        env['CXXFLAGS'] = f'{env.get("CXXFLAGS", "")} -DVERSION_INFO=\\"{self.distribution.get_version()}\\"'
+        
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
+
+        # 注意：我们移除了 -Dpybind11_DIR 和 -DPYTHON_EXECUTABLE
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 setup(
     name='shape_based_matching_py',
-    version='0.0.1',
-    author='meiqua',
-    author_email='rfjiang@qq.com',
-    description='py binding for shape based matching',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A Python wrapper for shape based matching',
     long_description='',
     ext_modules=[CMakeExtension('shape_based_matching_py')],
-    cmdclass=dict(build_ext=CMakeBuild),
+    cmdclass={'build_ext': CMakeBuild},
     zip_safe=False,
+    python_requires=">=3.8",
 )
